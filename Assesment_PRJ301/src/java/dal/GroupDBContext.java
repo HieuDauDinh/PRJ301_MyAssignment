61d600c8607736ccbdb260ee316594a9468e7cd0
HieuDauDinh <162103162+HieuDauDinh@users.noreply.github.com>
2024-03-04 15:56:22 +0700

    GetSessionByGid
    User to get attendance of teach in a group

--- Assesment_PRJ301/src/java/dal/GroupDBContext.java
+++ Assesment_PRJ301/src/java/dal/GroupDBContext.java
@@ -4,19 +4,32 @@
  */
 package dal;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Group;
+import model.Lecturer;
+import model.Room;
+import model.Session;
+import model.Slot;
+import model.Subject;
+import model.TimeSlot;
 
 /**
  *
  * @author Admin
  */
 public class GroupDBContext extends DBContext {
-
+    public static void main(String[] args) {
+        GroupDBContext db = new GroupDBContext();
+        int s = db.getSessionByGid("1").size();
+        System.out.println(s);
+    }
     public Group getGroupByGid(String gid) {
         SubjectDBContext sub = new SubjectDBContext();
         LecturerDBContext lec = new LecturerDBContext();
@@ -37,7 +50,7 @@ public class GroupDBContext extends DBContext {
                 String subid = rs.getString("Subid");
                 g.setSubid(sub.getSubBySubid(subid));
                 String lecid = rs.getString("PIC");
-                g.setLid(lec.getLecByLid(lecid));               
+                g.setLid(lec.getLecByLid(lecid));
                 return g;
             }
         } catch (SQLException ex) {
@@ -45,5 +58,57 @@ public class GroupDBContext extends DBContext {
         }
         return null;
 
+    }
+
+    public List<Session> getSessionByGid(String grid) {
+        List<Session> ses = new ArrayList<>();
+        String sql = "SELECT [seid]\n"
+                + "      ,[gid]\n"
+                + "      ,[lid]\n"
+                + "      ,[roomid]\n"
+                + "      ,[tid]\n"
+                + "      ,[isTaken]\n"
+                + "      ,[slotid]\n"
+                + "      ,[subid]\n"
+                + "      ,[date]\n"
+                + "  FROM [Assignment_PRJ301].[dbo].[Session]\n"
+                + "  Where gid = ?";
+        
+        List<Session> list = new ArrayList();
+        GroupDBContext group = new GroupDBContext();
+        LecturerDBContext lec = new LecturerDBContext();
+        TimeSlotDBContext tslot = new TimeSlotDBContext();
+        RoomDBContext room = new RoomDBContext();
+        SlotDBContext slot = new SlotDBContext();
+        SubjectDBContext subject = new SubjectDBContext();
+      
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, grid);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                int sid = rs.getInt("seid");
+                String gid = rs.getString("gid");
+                String lid = rs.getString("lid");
+                int tid = rs.getInt("tid");
+                boolean isTaken = rs.getBoolean("isTaken");
+                int slotid = rs.getInt("slotid");
+                String roomid = rs.getString("roomid");
+                String subid = rs.getString("subid");
+                Date date = rs.getDate("date");
+
+                Group gr = group.getGroupByGid(gid);
+                Lecturer l = lec.getLecByLid(lid);
+                Room r = room.getRoomByRid(roomid);
+                TimeSlot time = tslot.getTimeSlotByTid(tid);
+                Slot s = slot.getSlotBySlotId(slotid);
+                Subject sub = subject.getSubBySubid(subid);
+                Session sess = new Session(sid, gr, l, r, time, isTaken, s, sub, date);
+                ses.add(sess);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return ses;
     }
 }
